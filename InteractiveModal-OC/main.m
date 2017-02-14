fa674b53e4a0cfaaa4c3f2fda65a5d949ae1023e
shun <wangshun8888@gmail.com>
2017-02-14 15:29:54 +0800

    Initial Commit

---
+++ InteractiveModal-OC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InteractiveModal-OC
+//
+//  Created by 王顺 on 2017/2/14.
+//  Copyright © 2017年 shun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
