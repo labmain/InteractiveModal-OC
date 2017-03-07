b7b69a133a6d965388e6ad06150fc03b8eaf7478
shun <wangshun8888@gmail.com>
2017-03-07 21:38:27 +0800

    完成

--- InteractiveModal-OC/ModalViewController.m
+++ InteractiveModal-OC/ModalViewController.m
@@ -9,11 +9,8 @@
 #import "ModalViewController.h"
 
 @interface ModalViewController () <UITableViewDelegate, UITableViewDataSource, UIGestureRecognizerDelegate>
-/**
- tableview
-*/
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-
+@property (nonatomic, assign) CGFloat scrollProgress;
 @end
 
 @implementation ModalViewController
@@ -23,7 +20,6 @@
     [self prepareTableView];
 }
 - (void)prepareTableView{
-    // 注册 cellID
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cellID"];
     UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(tableViewGesture:)];
     pan.delegate = self;
@@ -31,7 +27,13 @@
 }
 
 - (void)tableViewGesture:(UIPanGestureRecognizer *)gesture {
-//    NSLog(@"offsetY : %f ", self.tableView.contentOffset.y);
+    
+    if (self.tableView.contentOffset.y > 0 && self.scrollProgress == 0) {
+        [gesture setTranslation:CGPointMake(0, 0) inView:self.view];
+        [self.interactor cancelInteractiveTransition];
+        return;
+    }
+    // 超过区域的百分比，就dismiss
     CGFloat percentThreshold = 0.3;
     
     CGPoint translation = [gesture translationInView:self.view];
@@ -39,18 +41,14 @@
     float downwardMovement = fmaxf(verticalMovement, 0.0);
     float downwardMovementPercent = fminf(downwardMovement, 1.0);
     CGFloat progress = downwardMovementPercent;
-    
-    NSLog(@"progress : %f", progress);
+    self.scrollProgress = progress;
     if (self.interactor == nil) {
         return;
     }
+    NSLog(@"progress : %f contentOffseY : %f", progress, self.tableView.contentOffset.y);
 
-    if (self.tableView.contentOffset.y > 0) {
-        [gesture setTranslation:CGPointMake(0, 0) inView:self.view];
-        progress = 0;
-//        for (UIGestureRecognizer *gesture in self.tableView.gestureRecognizers) {
-//            NSLog(@"%ld",gesture.state);
-//        }
+    if (progress > 0) {
+        [self.tableView setContentOffset:CGPointMake(0, 0)];
     }
     
     switch (gesture.state) {
@@ -60,14 +58,10 @@
             [self dismissViewControllerAnimated:YES completion:nil];
         }
             break;
-        case UIGestureRecognizerStateChanged:{
+        case UIGestureRecognizerStateChanged:
+        {
             self.interactor.shouldFinish = progress > percentThreshold;
-            
             [self.interactor updateInteractiveTransition:progress];
-            if (progress > 0) {
-                [self.tableView setContentOffset:CGPointMake(0, 0)];
-            }
-            
         }
             break;
         case UIGestureRecognizerStateCancelled:{
@@ -83,9 +77,8 @@
         default:
             break;
     }
-
+    
 }
-
 - (IBAction)closeBtnClick:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
@@ -130,60 +123,30 @@
             break;
     }
 }
-
+// 手势穿透
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
     if ([otherGestureRecognizer.view isKindOfClass:[UITableView class]]) {
-        NSLog(@"dfdf");
         return YES;
     }
     return NO;
 }
-//- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
-//    self.interactor.hasStarted = YES;
-//    [self dismissViewControllerAnimated:YES completion:nil];
-// 
-//}
-//
-//- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
-//    
-//    CGFloat percentThreshold = 0.3;
-//    
-//    CGPoint translation = CGPointMake(0, - scrollView.contentOffset.y);
-//    CGFloat verticalMovement = translation.y / self.tableView.bounds.size.height;
-//    float downwardMovement = fmaxf(verticalMovement, 0.0);
-//    float downwardMovementPercent = fminf(downwardMovement, 1.0);
-//    CGFloat progress = downwardMovementPercent;
-//    
-//    self.interactor.shouldFinish = progress > percentThreshold;
-//    NSLog(@"progress : %f", progress);
-//    [self.interactor updateInteractiveTransition:progress];
-//}
-//- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
-//
-//    self.interactor.hasStarted = NO;
-//    self.interactor.shouldFinish ? [self.interactor finishInteractiveTransition] : [self.interactor cancelInteractiveTransition];
-//}
-#pragma mark - UITableViewDataSource
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return 20;
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
-    // 根据 cellID 创建对应的 cell
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellID" forIndexPath:indexPath];
-    
     cell.backgroundColor = [UIColor orangeColor];
-    cell.textLabel.text = @"测试UITableViewCell";
+    cell.textLabel.text = [NSString stringWithFormat:@"Cell Index：%zd",indexPath.row];
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(nonnull NSIndexPath *)indexPath{
-    // 选中后立即取消选中效果
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return 44;
+    return 50;
 }
 
 #pragma mark -
